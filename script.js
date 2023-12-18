c04e6a9825f569d02f0d77ef79d61513b27d2b1e
Jamie Bradshaw <bradshJ@outlook.com>
2023-12-18 14:20:13 +0000

    Shoot and bigger box

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 // script.js
-// JavaScript code for the Box Moving Game
+// JavaScript code for the Space Shooter Game
 
+const gameContainer = document.getElementById("game-container");
 const player = document.getElementById("player");
-let playerLeft = 0;
-let playerTop = 0;
+let playerLeft = gameContainer.clientWidth / 2;
+let playerTop = gameContainer.clientHeight / 2;
+let bullets = [];
 
 document.addEventListener("keydown", movePlayer);
+document.addEventListener("keyup", shoot);
 
 function movePlayer(event) {
-    const speed = 10;
+    const speed = 5;
 
     switch (event.key) {
         case "ArrowUp":
@@ -31,4 +34,58 @@ function movePlayer(event) {
 function updatePlayerPosition() {
     player.style.left = playerLeft + "px";
     player.style.top = playerTop + "px";
+}
+
+function shoot(event) {
+    if (event.key === " ") {
+        const bullet = document.createElement("div");
+        bullet.className = "bullet";
+        bullet.style.left = playerLeft + "px";
+        bullet.style.top = playerTop + "px";
+        gameContainer.appendChild(bullet);
+
+        bullets.push(bullet);
+
+        // Move the bullet in the direction the player is facing
+        const bulletSpeed = 8;
+        const direction = getDirection();
+        const interval = setInterval(() => {
+            moveBullet(bullet, direction);
+        }, 20);
+
+        // Remove the bullet after it goes off-screen
+        setTimeout(() => {
+            clearInterval(interval);
+            gameContainer.removeChild(bullet);
+            bullets = bullets.filter(b => b !== bullet);
+        }, 2000);
+    }
+}
+
+function moveBullet(bullet, direction) {
+    switch (direction) {
+        case "up":
+            bullet.style.top = parseInt(bullet.style.top) - 5 + "px";
+            break;
+        case "down":
+            bullet.style.top = parseInt(bullet.style.top) + 5 + "px";
+            break;
+        case "left":
+            bullet.style.left = parseInt(bullet.style.left) - 5 + "px";
+            break;
+        case "right":
+            bullet.style.left = parseInt(bullet.style.left) + 5 + "px";
+            break;
+    }
+}
+
+function getDirection() {
+    const vertical = playerTop - gameContainer.clientHeight / 2;
+    const horizontal = playerLeft - gameContainer.clientWidth / 2;
+
+    if (Math.abs(vertical) > Math.abs(horizontal)) {
+        return vertical > 0 ? "up" : "down";
+    } else {
+        return horizontal > 0 ? "left" : "right";
+    }
 }
